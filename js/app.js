207843713c8f1843704ffcbb5667a3ae0f2d33ef
smarbos <smarbos@gmail.com>
2016-01-20 20:18:09 -0300

    Created Capitalize function. Also started the portfolio functionality and integrated it with the menu functionality

--- js/app.js
+++ js/app.js
@@ -1,44 +1,46 @@
 'use strict';
 
 var moravitaliApp = angular.module('moravitaliApp', ['ui.router', 'ui.bootstrap']);
-console.log("hola");
-
+moravitaliApp.filter('capitalize', function() {
+  return function(input, scope) {
+    if (input!=null)
+    input = input.toLowerCase();
+    return input.substring(0,1).toUpperCase()+input.substring(1);
+  }
+});
 moravitaliApp.config(function($stateProvider, $urlRouterProvider) {
 
-    $urlRouterProvider.otherwise('/home');
+    $urlRouterProvider.otherwise('/main');
 
     $stateProvider
 
 
-        .state('home', {
-            url: '/home',
-            templateUrl: 'views/home.html'
+        .state('main', {
+            url: '/main',
+            controller: 'main',
+            templateUrl: 'views/main.html'
         })
-        .state('mision', {
-            url: '/mision',
-            templateUrl: 'views/mision.html'
+        .state('portfolio', {
+            controller: 'portfolio',
+            url: '/portfolio',
+            templateUrl: 'views/portfolio.html'
         })
-        .state('rescatados', {
-            url: '/rescatados',
-            templateUrl: 'views/rescatados.html'
+        .state('lovestory', {
+            url: '/lovestory',
+            parent:'portfolio',
+            templateUrl: 'views/lovestory.html'
         })
-        .state('historias', {
-            url: '/historias',
-            templateUrl: 'views/historias.html'
+        .state('bio', {
+            url: '/bio',
+            templateUrl: 'views/bio.html'
         })
-        .state('shop', {
-            url: '/shop',
-            templateUrl: 'views/shop.html'
+        .state('cv', {
+            url: '/cv',
+            templateUrl: 'views/cv.html'
         })
         .state('contacto', {
             url: '/contacto',
+            controller: 'contacto',
             templateUrl: 'views/contacto.html'
-        })
-        .state('donaciones', {
-            url: '/donaciones',
-            templateUrl: 'views/donaciones.html'
         });
-
-        $urlRouterProvider.otherwise('/home');
-
 });
