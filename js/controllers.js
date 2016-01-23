a92c8d2da486cdffac6a00cca76f42880d7860d6
smarbos <smarbos@gmail.com>
2016-01-23 00:27:54 -0300

    Agregadas imagenes a la galeria, tambien ajustadas funciones de jquery para adaptar el tamaño del menu y del contenido al tamaño de la ventana

--- js/controllers.js
+++ js/controllers.js
@@ -3,16 +3,40 @@
 moravitaliApp.controller('body', ['$scope', '$location',
     function ($scope, $location) {
 
+        //GIVE THE WRAPER 50PX margin top and bottom
+        var element = $("#wraper");
+        var windowHeight = $(window).height();
+        var result = (windowHeight-250);
+        element.css({
+            height: result
+        });
+
+        //GIVE THE SIDEMENU 50PX margin top and bottom
+        var element = $("#sideMenu");
+
+        var elementHeight = $("#sideMenu").height();
+        var windowHeight = $(window).height();
+        var result = (windowHeight-50);
+        element.css({
+            height: result
+        });
+        console.log(element.height());
+
+
 
             $scope.mainTitle = "Mora Vitali Fotografa.";
 
             $scope.showChilds = function(item){
+            if(item.subMenu){
             item.active = !item.active;
+            }
+
             };
 
             $scope.items = [
             {
                 name: "portfolio",
+                subMenu: true,
                 subItems: [
                     {
                         name: "a simple love story",
@@ -125,34 +149,208 @@ moravitaliApp.controller('body', ['$scope', '$location',
                     },
                     {
                         name: "novia en rojo",
-                        ref: "noviaenrojo"
+                        ref: "noviaenrojo",
+                        items: [{
+                            id: 1,
+                            type: "img",
+                            titulo: "Titulo 1",
+                            content: "http://moravitali.com.ar/photo/noviaenrojo_1.jpg",
+                            thumbnail: "http://moravitali.com.ar/photo/noviaenrojo_1_thumb.jpg"
+                        },
+                        {
+                            id: 2,
+                            type: "img",
+                            titulo: "Titulo 2",
+                            content: "http://moravitali.com.ar/photo/noviaenrojo_2.jpg",
+                            thumbnail: "http://moravitali.com.ar/photo/noviaenrojo_2_thumb.jpg"
+                        },
+                        {
+                            id: 3,
+                            type: "img",
+                            titulo: "Titulo 3",
+                            content: "http://moravitali.com.ar/photo/noviaenrojo_3.jpg",
+                            thumbnail: "http://moravitali.com.ar/photo/noviaenrojo_3_thumb.jpg"
+                        },
+                        {
+                            id: 4,
+                            type: "img",
+                            titulo: "Titulo 4",
+                            content: "http://moravitali.com.ar/photo/noviaenrojo_4.jpg",
+                            thumbnail: "http://moravitali.com.ar/photo/noviaenrojo_4_thumb.jpg"
+                        },
+                        {
+                            id: 5,
+                            type: "img",
+                            titulo: "Titulo 5",
+                            content: "http://moravitali.com.ar/photo/noviaenrojo_5.jpg",
+                            thumbnail: "http://moravitali.com.ar/photo/noviaenrojo_5_thumb.jpg"
+                        },
+                        {
+                            id: 6,
+                            type: "img",
+                            titulo: "Titulo 6",
+                            content: "http://moravitali.com.ar/photo/noviaenrojo_6.jpg",
+                            thumbnail: "http://moravitali.com.ar/photo/noviaenrojo_6_thumb.jpg"
+                        }]
                     },
                     {
                         name: "toy cams",
-                        ref: "toycams"
+                        ref: "toycams",
+                        items: [{
+                            id: 1,
+                            type: "img",
+                            titulo: "Titulo 1",
+                            content: "http://moravitali.com.ar/photo/toycams_1.JPG",
+                            thumbnail: "http://moravitali.com.ar/photo/toycams_1_thumb.jpg"
+                        },
+                        {
+                            id: 2,
+                            type: "img",
+                            titulo: "Titulo 2",
+                            content: "http://moravitali.com.ar/photo/toycams_2.JPG",
+                            thumbnail: "http://moravitali.com.ar/photo/toycams_2_thumb.jpg"
+                        },
+                        {
+                            id: 3,
+                            type: "img",
+                            titulo: "Titulo 3",
+                            content: "http://moravitali.com.ar/photo/toycams_3.JPG",
+                            thumbnail: "http://moravitali.com.ar/photo/toycams_3_thumb.jpg"
+                        },
+                        {
+                            id: 4,
+                            type: "img",
+                            titulo: "Titulo 4",
+                            content: "http://moravitali.com.ar/photo/toycams_4.JPG",
+                            thumbnail: "http://moravitali.com.ar/photo/toycams_4_thumb.jpg"
+                        },
+                        {
+                            id: 5,
+                            type: "img",
+                            titulo: "Titulo 5",
+                            content: "http://moravitali.com.ar/photo/toycams_5.JPG",
+                            thumbnail: "http://moravitali.com.ar/photo/toycams_5_thumb.jpg"
+                        }]
                     },
                     {
                         name: "las mo(i)ras",
-                        ref: "lasmoiras"
+                        ref: "lasmoiras",
+                        items: [{
+                            id: 1,
+                            type: "img",
+                            titulo: "Titulo 1",
+                            content: "http://moravitali.com.ar/photo/las_moiras_1.jpg",
+                            thumbnail: "http://moravitali.com.ar/photo/las_moiras_1_thumb.jpg"
+                        },
+                        {
+                            id: 2,
+                            type: "img",
+                            titulo: "Titulo 2",
+                            content: "http://moravitali.com.ar/photo/las_moiras_2.jpg",
+                            thumbnail: "http://moravitali.com.ar/photo/las_moiras_2_thumb.jpg"
+                        },
+                        {
+                            id: 3,
+                            type: "img",
+                            titulo: "Titulo 3",
+                            content: "http://moravitali.com.ar/photo/las_moiras_3.jpg",
+                            thumbnail: "http://moravitali.com.ar/photo/las_moiras_3_thumb.jpg"
+                        },
+                        {
+                            id: 4,
+                            type: "img",
+                            titulo: "Titulo 4",
+                            content: "http://moravitali.com.ar/photo/las_moiras_4.jpg",
+                            thumbnail: "http://moravitali.com.ar/photo/las_moiras_4_thumb.jpg"
+                        },
+                        {
+                            id: 5,
+                            type: "img",
+                            titulo: "Titulo 5",
+                            content: "http://moravitali.com.ar/photo/las_moiras_5.jpg",
+                            thumbnail: "http://moravitali.com.ar/photo/las_moiras_5_thumb.jpg"
+                        },
+                        {
+                            id: 6,
+                            type: "img",
+                            titulo: "Titulo 6",
+                            content: "http://moravitali.com.ar/photo/las_moiras_6.jpg",
+                            thumbnail: "http://moravitali.com.ar/photo/las_moiras_6_thumb.jpg"
+                        },
+                        {
+                            id: 7,
+                            type: "img",
+                            titulo: "Titulo 7",
+                            content: "http://moravitali.com.ar/photo/las_moiras_7.jpg",
+                            thumbnail: "http://moravitali.com.ar/photo/las_moiras_7_thumb.jpg"
+                        },
+                        {
+                            id: 8,
+                            type: "img",
+                            titulo: "Titulo 8",
+                            content: "http://moravitali.com.ar/photo/las_moiras_8.jpg",
+                            thumbnail: "http://moravitali.com.ar/photo/las_moiras_8_thumb.jpg"
+                        }]
                     },
                     {
                         name: "arte contra la trata",
-                        ref: "artecontralatrata"
+                        ref: "artecontralatrata",
+                        items: [{
+                            id: 1,
+                            type: "img",
+                            titulo: "Titulo 1",
+                            content: "http://moravitali.com.ar/photo/alameda_1.jpg",
+                            thumbnail: "http://moravitali.com.ar/photo/alameda_1_thumb.jpg"
+                        },
+                        {
+                            id: 2,
+                            type: "img",
+                            titulo: "Titulo 2",
+                            content: "http://moravitali.com.ar/photo/alameda_2.jpg",
+                            thumbnail: "http://moravitali.com.ar/photo/alameda_2_thumb.jpg"
+                        },
+                        {
+                            id: 3,
+                            type: "img",
+                            titulo: "Titulo 3",
+                            content: "http://moravitali.com.ar/photo/alameda_3.jpg",
+                            thumbnail: "http://moravitali.com.ar/photo/alameda_3_thumb.jpg"
+                        }]
                     }
                 ]
             },
             {
                 name: "bio",
                 ref: "bio",
-                subItems: []
+                subMenu: false,
+                subItems: [{
+                    id: 1,
+                    aniomuestra: "Titulo 1",
+                    textomuestra: "texto-muestra 1 texto-muestra 1 texto-muestra 1",
+                    linkmuestra: "http://moravitali.com.ar/photo/asimplelovestori_01_thumb.jpg"
+                },
+                {
+                    id: 2,
+                    aniomuestra: "Titulo 2",
+                    textomuestra: "texto-muestra 2 texto-muestra 2 texto-muestra 2",
+                    linkmuestra: "http://moravitali.com.ar/photo/asimplelovestori_01_thumb.jpg"
+                },
+                {
+                    id: 3,
+                    aniomuestra: "Titulo 1",
+                    textomuestra: "texto-muestra 3 texto-muestra 3 texto-muestra 3",
+                    linkmuestra: "http://moravitali.com.ar/photo/asimplelovestori_01_thumb.jpg"
+                }]
             },
             {
                 name: "cv",
+                subMenu: false,
                 ref: "cv",
                 subItems: []
             },
             {
                 name: "contacto",
+                subMenu: false,
                 ref: "contacto",
                 subItems: []
             }
@@ -160,55 +358,32 @@ moravitaliApp.controller('body', ['$scope', '$location',
     }
 ]);
 
+
+
 moravitaliApp.controller('main', ['$scope', '$location',
     function ($scope, $location) {
-        function centerPlease(element,margin){
-            element.load(function(){
-                var windowHeight = $(window).height();
-                var windowWidth = $(window).width();
-                if(windowWidth<1600){
-                    margin = margin*2;
-                }
-                var maxHeight = windowHeight-(margin*2);
-                element.height(maxHeight+'px');
-                var imageHeight = $(this).height();
-                var imageWidth = $(this).width();
-                var marginAlto = (windowHeight - imageHeight)/2;
-                var marginAncho = (windowWidth - imageWidth)/2;
-                element.offset({ top: marginAlto, left: marginAncho})
-            });
-        }
-        var margin = "50";
-        centerPlease($("#mainImg"),margin);
+
+        //ADJUST THE MAIN IMAGE
+        var element = $("#mainImg");
+        var target = $("#wraper");
+        console.log(element, target);
+        var elementHeight = element.height();
+        var targetHeight = target.height();
+        element.css({
+            height: targetHeight
+        });
     }
 ]);
 
 moravitaliApp.controller('portfolio', ['$scope', '$location', '$stateParams',
     function ($scope, $location, $stateParams) {
-        function centerPlease(element,margin){
-            var windowHeight = $(window).height();
-            var windowWidth = $(window).width();
-            if(windowWidth<1600){
-                margin = margin*2;
-            }
-            var maxHeight = windowHeight-(margin*2);
-            element.height(maxHeight+'px');
-            var imageHeight = element.height();
-            var imageWidth = element.width();
-            var marginAlto = (windowHeight - imageHeight)/2;
-            var marginAncho = (windowWidth - imageWidth)/2;
-                element.offset({ top: marginAlto, left: marginAncho})
-        }
-        var margin = "50";
-        centerPlease($("#gallery"),margin);
-        var i;
 
         //Creo el contenedor de las slides
         $scope.slides = [];
 
         //TODO: poder deshabilitar una galeria
         //Reviso que la galeria seleccionada tenga contenido
-
+        var i;
         for(i=0; i<$scope.items[0].subItems.length; i++){
             if($scope.items[0].subItems[i].ref === $location.$$path.split("/").pop()){
                 var e;
