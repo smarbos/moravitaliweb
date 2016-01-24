9eb05f19f7dc623b4418ed04d927ad1283bc1c2e
smarbos <smarbos@gmail.com>
2016-01-24 19:01:31 -0300

    Added PHPMailer to send mails from the contact form

--- js/controllers.js
+++ js/controllers.js
@@ -74,12 +74,44 @@ moravitaliApp.controller('portfolio', ['$scope', '$location', '$stateParams',
     }
 ]);
 
-moravitaliApp.controller('contacto', ['$scope', '$location',
-    function ($scope, $location) {
-        var windowHeight = $(window).height();
-        var contactHeight = $("#contacto").height();
-        var margin = (windowHeight - contactHeight)/2;
-        $("#contacto").attr('style','top:'+margin+'px;');
+moravitaliApp.controller('contacto', ['$scope', '$location', '$http',
+    function ($scope, $location, $http) {
+
+        $scope.result = 'hidden'
+    $scope.resultMessage;
+    $scope.formData; //formData is an object holding the name, email, subject, and message
+    $scope.submitButtonDisabled = false;
+    $scope.submitted = false; //used so that form errors are shown only after the form has been submitted
+    $scope.submit = function(contactform) {
+        event.preventDefault();
+        $scope.submitted = true;
+        $scope.submitButtonDisabled = true;
+        if (contactform.$valid) {
+            $http({
+                method  : 'POST',
+                url     : 'contact-form.php',
+                data    : $.param($scope.formData),  //param method from jQuery
+                headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  //set the headers so angular passing info as form data (not request payload)
+            }).success(function(data){
+                console.log(data);
+                if (data.success) { //success comes from the return json object
+                    $scope.submitButtonDisabled = true;
+                    $scope.resultMessage = data.message;
+                    $scope.result='bg-success';
+                } else {
+                    $scope.submitButtonDisabled = false;
+                    $scope.resultMessage = data.message;
+                    $scope.result='bg-danger';
+                }
+            });
+        } else {
+            $scope.submitButtonDisabled = false;
+            $scope.resultMessage = 'Failed <img src="http://www.chaosm.net/blog/wp-includes/images/smilies/icon_sad.gif" alt=":(" class="wp-smiley">  Please fill out all the fields.';
+            $scope.result='bg-danger';
+        }
+        console.log($scope.formData);
+
+    }
     }
 ]);
 
